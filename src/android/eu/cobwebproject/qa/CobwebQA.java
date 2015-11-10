3d56e23129c6abaaca9a3bb5c6f92ba992325566
George Hamilton <george.hamilton@ed.ac.uk>
2015-11-10 16:04:13 +0000

    replace tabs with spaces

--- src/android/eu/cobwebproject/qa/CobwebQA.java
+++ src/android/eu/cobwebproject/qa/CobwebQA.java
@@ -12,37 +12,37 @@ import android.util.Log;
 import eu.cobwebproject.qa.automaticvalidation.BlurCheckAndroid;
 
 public class CobwebQA extends CordovaPlugin {
-	
-    @Override
-	public boolean execute(String action,
-                           JSONArray args,
-                           final CallbackContext callbackContext) throws JSONException {
-        if(action.equals("isBlurred")){
-        	final String filePath = args.getString(0);
-        	final int threshold = args.getInt(1);
-			Log.i("CobwebQA", "Blur check on " + filePath + " with threshold " + threshold);
-            	
-        	cordova.getThreadPool().execute(new Runnable() {
-        		@Override
-        		public void run() {
-        			BlurCheckAndroid lap = new BlurCheckAndroid(
-        					new File(filePath),
-        					threshold,
-        					false,
-        					cordova.getActivity().getApplicationContext());
-        			lap.run();
 
-        			Log.i("CobwebQA", "Result: " + lap.pass);
-        			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, !lap.pass));
-        		}
-        	});
-        	
+    @Override
+    public boolean execute(String action,
+            JSONArray args,
+            final CallbackContext callbackContext) throws JSONException {
+        if(action.equals("isBlurred")){
+            final String filePath = args.getString(0);
+            final int threshold = args.getInt(1);
+            Log.i("CobwebQA", "Blur check on " + filePath + " with threshold " + threshold);
+
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    BlurCheckAndroid lap = new BlurCheckAndroid(
+                            new File(filePath),
+                            threshold,
+                            false,
+                            cordova.getActivity().getApplicationContext());
+                    lap.run();
+
+                    Log.i("CobwebQA", "Result: " + lap.pass);
+                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, !lap.pass));
+                }
+            });
+
             return true;
         }
         else{
-        	// no such method
-        	Log.w("CobwebQA", "No method '" + action + "' found");
-        	return false;
+            // no such method
+            Log.w("CobwebQA", "No method '" + action + "' found");
+            return false;
         }
     }
 }
